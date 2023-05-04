e5f4f0d321ccbefc926e6968fc50d380e33ceb99
Qiuyang Wang <qiuyangwang@brandeis.edu>
2023-05-04 12:13:12 -0400

    updated poetry functionality

--- routes/poetry.js
+++ routes/poetry.js
@@ -20,4 +20,25 @@ router.get("/poetry", (req, res, next) => {
     res.render('gptpoetry', { title: 'Express' });
 })
 
+router.post('/poetry',
+isLoggedIn,
+async (req, res, next) => {
+    let answer = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Generate a romatic poem using poetic feelings with the following needs: "
+            + "style: " + req.body.style + ", "
+            + "theme: " + req.body.theme + ", "
+            + "language: " + req.body.language,
+        temperature: 1,
+        max_tokens: 2048,
+        n: 1,
+        stop: null,
+    })
+        .then(results => { return results.data.choices[0].text })
+        .catch(error => console.error(error));
+
+    res.render('poetryAnswer', { answer })
+}
+)
+
 module.exports = router;
